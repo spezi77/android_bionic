8c62c82da7af3556a55998ef08b1996fe102c3d3
Darren Smith <nerevar@gmail.com>
2015-07-01 21:20:56 -0500

    Fix previous cherry-pick
    Change-Id: I224dd0e80430afb31c75ce9e578581f36634cbe7

--- libc/arch-arm/scorpion/scorpion.mk
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -5,6 +5,7 @@ $(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/krait/bionic/memmove.S)
 $(call libc-add-cpu-variant-src,BCOPY,)
 # Use cortex-a15 versions of strcat/strcpy/strlen.
 $(call libc-add-cpu-variant-src,STRCAT,arch-arm/cortex-a15/bionic/strcat.S)
+$(call libc-add-cpu-variant-src,STPCPY,arch-arm/cortex-a15/bionic/stpcpy.S)
 $(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a15/bionic/strcpy.S)
 $(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a15/bionic/strlen.S)
 $(call libc-add-cpu-variant-src,__STRCAT_CHK,arch-arm/cortex-a15/bionic/__strcat_chk.S)
